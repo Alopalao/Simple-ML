ec9dac9b4d0fc0ff818dc7b2e1a207b48cfe2cfe
Aldo <aorte090@fiu.edu>
2022-01-03 03:52:39 -0500

    Fixed

--- machine.py
+++ machine.py
@@ -9,18 +9,23 @@ y = dataset["diagnosis(1=m, 0=b)"]
 
 #machine learning library to split data
 from sklearn.model_selection import train_test_split
-#test size is 20%
+
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 #Keras to build neural network
 import tensorflow as tf
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(256, input_shape=x_train.shape, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(256, input_shape=(x_train.shape[1],), activation='sigmoid'))
 model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid')) #answer -> diagnosis
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metric=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+#Additional code
+train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+valid_data = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
 #1000 to go over data a lot of times
+#model.fit(train_data, epochs=10, validation_data=valid_data)
 model.fit(x_train, y_train, epochs=1000)
